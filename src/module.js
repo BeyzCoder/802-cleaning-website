8c6adce1dc9e3ce21c72b4989627a74bf0277f5f
BeyzCoder <stevenbaes11@yahoo.com>
2024-10-01 18:51:56 -0600

    Change contacts

--- src/module.js
+++ src/module.js
@@ -14,7 +14,7 @@ import showcase7 from './images/showcase-7.jpg';
 
 // Contacts Informations.
 export const address = "802 Gray Ave, Saskatoon, SK S7N 2J4";
-export const phoneNumber = "(306) 774-2863";
+export const phoneNumber = "(306) 717-2080";
 export const email = "802windowcleaning@gmail.com";
 
 // Home Page.
